d26623f723d993c4fd6f7588981cdf8f6994e6ca
Christhoper Portillo <christhoperportillo@Mac-mini-de-Christhoper.local>
2021-05-13 16:47:43 -0600

    avance

---
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey: string = 'CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial(){
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient){
+
+  }
+
+  buscarGifs( query: string ){
+    
+    query = query.trim().toLocaleLowerCase();
+
+    if( !this._historial.includes( query ) ){
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0,10);
+    }
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5&q=${query}&limit=10`)
+    .subscribe( (resp ) => {
+      console.log(resp.data);
+      this.resultados = resp.data;
+    });
+    
+    
+  }
+}
