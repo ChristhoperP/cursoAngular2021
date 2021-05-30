a74185c1c424b69ea7adce01bd9609b1b9298bdd
Christhoper <christhoper@rhinorealtyinc.com>
2021-05-30 00:54:49 -0600

    fin seccion 25

--- 12-auth-server/controllers/auth.js
+++ 12-auth-server/controllers/auth.js
@@ -41,6 +41,7 @@ const crearUsuario = async (req, res = response) => {
             ok: true,
             uid: dbUser.id,
             name,
+            email,
             msg: 'Se creo el usuario exitosamente.',
             token
         });
@@ -85,6 +86,7 @@ const loginUsuario = async(req, res = response) => {
             ok: true,
             uid: dbUser.id,
             name: dbUser.name,
+            email,
             token
         })
 
@@ -100,16 +102,20 @@ const loginUsuario = async(req, res = response) => {
 
 const revalidarToken = async(req, res = response) => {
 
-    const {uid, name} = req;
+    const {uid} = req;
+
+    //Leer la base de datos
+    const dbUser = await Usuario.findById(uid);
 
     // Generar el JWT
-    const token = await generarJWT(uid, name);
+    const token = await generarJWT(uid, dbUser.name);
 
     return res.json({
         ok: true,
         msg: 'Renew',
         uid,
-        name,
+        name: dbUser.name,
+        email: dbUser.email,
         token
     })
 };
